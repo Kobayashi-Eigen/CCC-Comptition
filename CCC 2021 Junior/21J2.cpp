37abc293200586f45d2ef0421aa01cc34681c2ff
Lawrence Lim <54785833+Kobayashi-Eigen@users.noreply.github.com>
2022-07-25 11:06:12 +0800

    Add files via upload

---
+++ CCC 2021 Junior/21J2.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<string>
+using namespace std;
+int main()
+{
+	int n,price=0,winner_price;
+	string name,winner;
+	cin>>n;
+	for(int i=0;i<n;i++)
+	{
+		cin>>name>>price;
+		if(price>winner_price)
+		{
+			winner = name;
+			winner_price=price;
+		}
+	}
+	cout<<winner;
+	return 0;
+} 
